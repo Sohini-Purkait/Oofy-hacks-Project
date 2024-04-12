49fdcc5b55ccec47be577acac3ca5b2e78b18b27
Tuhin Chandra <tuhinchandra2k04@gmail.com>
2024-04-12 22:26:20 +0530

    Added the navbar and hero section of Services page

---
+++ script2.js
@@ -0,0 +1,7 @@
+const loginBtn = document.querySelector(".text")
+const form = document.querySelector(".form")
+const body = document.querySelector("html")
+
+function toggling(){
+    form.classList.toggle("form-show");
+}

6b3d54fb1553415f285e4bc722a47fceef2491de
Tuhin Chandra <tuhinchandra2k04@gmail.com>
2024-04-13 14:59:23 +0530

    Added 3 more sections after hero section

--- script2.js
+++ script2.js
@@ -2,6 +2,24 @@ const loginBtn = document.querySelector(".text")
 const form = document.querySelector(".form")
 const body = document.querySelector("html")
 
-function toggling(){
+function toggling() {
     form.classList.toggle("form-show");
 }
+
+const leftBtn = document.querySelector(".slider-left button")
+const rightBtn = document.querySelector(".slider-right button")
+let num = 0
+rightBtn.addEventListener("click", () => {
+    if (num % 2 === 0) {
+        document.querySelector(".card-center1").classList.toggle("hide-card-center1")
+        document.querySelector(".card-center6").classList.toggle("show-card-center6")
+        num+=1
+    }
+})
+leftBtn.addEventListener("click",()=>{
+    if(num%2===1){
+        document.querySelector(".card-center1").classList.toggle("hide-card-center1")
+        document.querySelector(".card-center6").classList.toggle("show-card-center6")
+        num+=1
+    }
+})
